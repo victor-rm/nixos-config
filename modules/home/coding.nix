d530adc0c2635a70e93f6a94b238c0f345663efe
Victor Ribeiro <victruu@outlook.com>
2026-01-03 06:02:35 -0300

    feat: adicionando direnv

--- modules/home/coding.nix
+++ modules/home/coding.nix
@@ -23,6 +23,12 @@
     };
   };
 
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  }
+
   home.packages = with pkgs; [
     vscodium
     nodejs_20
